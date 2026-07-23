a3ec60609529ac15ed5fb1ebf4bf79151174945b
adarsh-devx <devx.adarshh@gmail.com>
2026-07-23 14:11:22 +0530

    day-4

---
+++ day-4/src/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json())
+
+
+const notes = []
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+
+app.post('/notes' , (req , res) => {
+    console.log(req.body);
+    notes.push(req.body)
+
+    console.log(notes);
+    
+    res.send('note created')
+    
+})
+
+app.get('/notes' , (req , res) => {
+    res.send(notes)
+})
+
+app.delete('/notes/:id' , (req , res) => {
+    delete notes[req.params.id]
+    res.send('note deleted')
+    
+    
+})
+
+
+app.patch('/notes/:id' , (req , res) => {
+    
+    notes[req.params.id].description = req.body.description
+    res.send('note updated')
+
+    
+})
+
+module.exports = app;
