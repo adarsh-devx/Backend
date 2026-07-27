996b0d8de7b7dc9cbb3178d0c896d053a053f16d
adarsh-devx <devx.adarshh@gmail.com>
2026-07-27 22:23:20 +0530

    feat: initialize express application with cookie-parser, body parsing, and auth routes

--- day-9/src/app.js
+++ day-9/src/app.js
@@ -1,22 +1,14 @@
-const express = require('express')
-const authRouter = require('./routes/auth.routes')
+const express = require("express");
+const authRouter = require("./routes/auth.routes");
 
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
+const app = express();
 
+app.use(cookieParser());
 
-const app = express()
+app.use(express.json());
 
-app.use(cookieParser())
-
-app.use(express.json())
-
-
-
-app.use('/api/auth', authRouter)
-
-
-
-
+app.use("/api/auth", authRouter);
 
 module.exports = app;
