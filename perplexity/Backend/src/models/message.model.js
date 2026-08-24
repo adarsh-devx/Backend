b295d03a207429a395e274c2d57c9add3763859c
adarsh-devx <devx.adarshh@gmail.com>
2026-08-25 03:38:39 +0530

    add user, chat, and message models

---
+++ perplexity/Backend/src/models/message.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    chat: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: {
+        values: ["user", "ai"],
+        message: "{VALUE} is not a valid role",
+      },
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const messageModel = mongoose.model("Message", messageSchema);
+export default messageModel;
---
+++ perplexity/Backend/src/models/user.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+
+
+
+const userModel = mongoose.model("User", userSchema);
+export default userModel;
