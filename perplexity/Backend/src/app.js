2879bd55537ea70ffb23932edacd8edc3a90d026
adarsh-devx <devx.adarshh@gmail.com>
2026-08-27 21:03:36 +0530

    setup: config CORS for frontend port and clean up duplicate middlewares

--- perplexity/Backend/src/app.js
+++ perplexity/Backend/src/app.js
@@ -2,16 +2,17 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
+import morgan from "morgan";
 
 const app = express();
 
 // --- Built-in middleware ---
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true , methods: ["GET", "POST", "PUT", "DELETE"] }));
+app.use(morgan("dev"));
 
 // --- Routes ---
-
 app.use("/api/auth", authRouter);
 
 export default app;
