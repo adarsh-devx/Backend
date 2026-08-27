2e34b3ad4c9e7bdf90eda903371d4cca7225543a
adarsh-devx <devx.adarshh@gmail.com>
2026-08-27 22:32:05 +0530

    setup: initialize socket.io server and integrate with HTTP server

--- perplexity/Backend/server.js
+++ perplexity/Backend/server.js
@@ -5,7 +5,14 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 import "dotenv/config"
 import app from "./src/app.js";
+import http from "http"
+import { initSocket } from "./src/sockets/server.socket.js";
+
 import connectDB from "./src/config/database.js";
+
+
+const httpServer = http.createServer(app);
+initSocket(httpServer);
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,6 +23,6 @@ connectDB()
 });
 
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
