4f1d85cd4c2ff67b2bef0590be584c32c9dfd529
adarsh-devx <devx.adarshh@gmail.com>
2026-08-28 06:18:38 +0530

    ai: map conversation history to gemini and add mistral title generator

--- perplexity/Backend/src/services/ai.service.js
+++ perplexity/Backend/src/services/ai.service.js
@@ -1,14 +1,40 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatMistralAI } from "@langchain/mistralai";
+import { HumanMessage, SystemMessage , AIMessage } from "langchain";
 
-const model = new ChatGoogleGenerativeAI({
-  model: "gemini-3.7-flash",
-  apiKey: process.env.GOOGLE_API_KEY,
+const geminiModel = new ChatGoogleGenerativeAI({
+  model: "gemini-3.6-flash",
+  apiKey: process.env.GEMINI_API_KEY,
 });
 
+const mistralModel = new ChatMistralAI({
+  model: "mistral-small-latest",
+  apiKey: process.env.MISTRAL_API_KEY,
+});
 
+export async function generateResponse(messages) {
+  const response = await geminiModel.invoke(messages.map(msg => {
+    if(msg.role === "user"){
+      return new HumanMessage(msg.content);
+    }else if(msg.role === "ai"){
+      return new AIMessage(msg.content);
+    }
+  }));
+  return response.text;
+}
 
-export async function testAi() {
-    model.invoke("hello !").then((response) => {
-        console.log(response.text);
-    });
+export async function generateChatTitle(message) {
+  const response = await mistralModel.invoke([
+    new SystemMessage(`You are a chat title generator. Generate a concise and descriptive title for the following chat conversation.
+
+    User will provide the first message of the chat conversation, and you will generate a title that captures the essence of the conversation in 2-4 words. The title should be clear, relevant , and engaging, giving users a quick understanding of the chat's topic.
+      
+      `),
+    new HumanMessage(`
+
+      Generate a title for a chat conversation based on the following first message: "${message}"
+      
+      `),
+  ]);
+  return response.text;
 }
